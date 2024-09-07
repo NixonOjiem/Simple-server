fa36925d88c741a9be977f1f4c28d6d020629723
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-07 22:56:41 +0300

    added one more row in API results

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,8 @@ router.get('/', function(req, res, next) {
     {id: 1, name: 'John', age: 25},
     {id: 2, name: 'Jane', age: 30},
     {id: 3, name: 'Bob', age: 35},
-    {id: 4, name: "Nicko", age: 27}
+    {id: 4, name: "Nicko", age: 27},
+    {id: 5, name: "Fatma", age: 20}
   ];
   // return results from API
   res.json({
